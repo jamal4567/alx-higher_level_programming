550a7f7f0dcc5baccc069b97cdd9f066b6edd1f0
jamal97 <jamalimmmiss97@gmail.com>
2023-09-25 06:43:42 -0700

    prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except (typeError, valueError):
+    except (ValueError):
         return False
