4d1b82dad5c4d644ba2b6118055712243d300466
jamal97 <jamalimmmiss97@gmail.com>
2023-09-26 08:52:41 -0700

    access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''defines a class square'''
+
+
+class Square:
+    ''' represent squar'''
+
+    def __init__(self, size=0):
+        '''inisializing
+        Args:
+            size (int): represent the size of class square.
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+    @property
+    def size(self):
+        ''' retrive the size '''
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''square area method
+        Return: current square area
+        '''
+
+        return (self.__size ** 2)
