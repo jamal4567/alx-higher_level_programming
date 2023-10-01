99ad4a2fea98d4516357eff9aa9c17787d00c58a
jamal97 <jamalimmmiss97@gmail.com>
2023-10-01 10:50:08 -0700

    TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+modul with one function that add 2 integers
+'''
+
+
+def add_integer(a, b=98):
+    ''' Return an integer the addition of a and b
+
+    Args:
+        a : an integer
+        b : an integer
+
+    Raises:
+        TypeError: if a or b not integer or float
+    '''
+
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise("b must be an integer")
+    return (int(a) + int(b))
