73518b39606b4c18280fcb08d6bd04632ecd6858
jamal97 <jamalimmmiss97@gmail.com>
2023-10-02 02:45:22 -0700

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
             return 0
         return 2 * (self.__height + self.__width)
     
-    def __str__(self):
+    def __str__(self) -> str:
         ''' print the rectangle with the character #
         using str and print
         '''
