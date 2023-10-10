a3c68d428c89620069dd7f027e2fbb2432725196
jamal97 <jamalimmmiss97@gmail.com>
2023-10-10 09:58:32 -0700

    task-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list, and then save them to a file"""
+""" Module add all arguments to a Python list, and then save them to a file """
 
 
 import sys
