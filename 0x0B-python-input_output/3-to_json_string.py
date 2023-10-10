626bbb97245a4f6d029e773e245228b1ff1290c7
jamal97 <jamalimmmiss97@gmail.com>
2023-10-10 07:53:18 -0700

    task-3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' JSON representation '''
+
+
+def to_json_string(my_obj):
+    ''' function that returns the JSON representation of an object '''
+
+    import json
+    return json.dumps(my_obj)
