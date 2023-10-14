a2d4a761dadd36a201b9102e71e4aca74aedc89f
jamal <jamalimmiss97@gmail.com>
2023-10-14 06:10:16 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' defines class base '''
+
+
+class Base:
+    ''' Represent class base '''
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
