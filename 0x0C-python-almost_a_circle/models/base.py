f6acf260b2d2bbe101a15852e47a55a6a47c376b
jamal <jamalimmiss97@gmail.com>
2023-10-16 06:12:39 -0700

    class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-''' defines class base '''
+''' defines class rectangle '''
+
+import json
 
 
 class Base:
@@ -11,3 +13,55 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' Returns the JSON string representation '''
+        if list_dictionaries is None or list_dictionaries == []:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' Writes the JSON string '''
+        filenam = cls.__name__+'.json'
+        a = []
+        if list_objs is not None:
+            for ele in list_objs:
+                a.append(cls.to_dictionary(ele))
+        with open(filenam, 'w') as f:
+            f.write(cls.to_json_string(a))
+
+    @staticmethod
+    def from_json_string(json_string):
+        ''' Returns the list of the JSON string '''
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        ''' Returns an instance with all attributes already set '''
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1,1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        ''' Returns a list of instances '''
+        filename = cls.__name__+'.json'
+        try:
+            with open(filename) as f:
+                l_dictionaries = cls.from_json_string(f.read())
+        except:
+            return []
+
+        l_isinstance = []
+        for dictionary in l_dictionaries:
+            l_isinstance.append(cls.create(**dictionary))
+        
+        return l_isinstance
