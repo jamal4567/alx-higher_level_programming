e6dcdc7464d0db517aacf80f865c4f921ab57e0d
jamal <jamalimmiss97@gmail.com>
2023-10-20 09:21:17 -0700

    class Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         self.__size = size
         super().__init__(size, size, x, y, id)
-    
+
     @property
     def size(self):
         ''' det value of size '''
@@ -23,9 +23,9 @@ class Square(Rectangle):
 
     def __str__(self):
         ''' Return string format '''
-        string = '[Square]' +' ('+str(self.id)+') '
-        string += str(self.x)+ '/' +str(self.y)
-        string += ' - ' +str(self.size)
+        string = '[Square]' + ' (' + str(self.id) + ') '
+        string += str(self.x) + '/' + str(self.y)
+        string += ' - ' + str(self.size)
         return string
 
     def update(self, *args, **kwargs):
@@ -52,10 +52,10 @@ class Square(Rectangle):
                     self.x = value
                 elif key == 'y':
                     self.y = value
-    
+
     def to_dictionary(self):
         ''' Return the dictionary representation of square '''
-        dictionary = {'id':self.id, 'size':self.__size,
-                      'x':self.x,
-                      'y':self.y}
+        dictionary = {'id': self.id, 'size': self.__size,
+                      'x': self.x,
+                      'y': self.y}
         return dictionary
