d91bc139cc7420bb871c7f83153620f22430b507
jamal4567 <jamalimmiss97@gmail.com>
2023-11-14 14:59:39 -0800

    task-9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- script that creates a table second_table in the database hbtn_0c_0
 --second_table description:
------ id INT
------ name VARCHAR(256)
------ score INT
+--    id INT
+--    name VARCHAR(256)
+--    score INT
 -- this script will create this recorde too:
---- id = 1, name = “John”, score = 10
---- id = 2, name = “Alex”, score = 3
---- id = 3, name = “Bob”, score = 14
---- id = 4, name = “George”, score = 8
+--  id = 1, name = “John”, score = 10
+--  id = 2, name = “Alex”, score = 3
+--  id = 3, name = “Bob”, score = 14
+--  id = 4, name = “George”, score = 8
 
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 name VARCHAR(256),
