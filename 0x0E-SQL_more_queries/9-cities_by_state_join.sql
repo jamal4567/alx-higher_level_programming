37275d0aa911d30a543dfaebc7f961725b1de451
jamal <jamalimmiss97@.com>
2023-12-13 21:53:41 +0000

    task-9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,5 +6,5 @@
 --     The database name will be passed as an argument of the mysql command
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id = states.state_id
+INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
