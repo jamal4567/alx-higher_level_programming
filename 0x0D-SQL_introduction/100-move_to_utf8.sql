73066e99cda25b84632509e7d6045cb9b83af720
jamal <jamalimmiss97@.com>
2023-12-14 13:53:21 +0000

    task-17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,6 +5,8 @@
 --   Database hbtn_0c_0
 --   Table first_table
 --   Field name in first_table
-ALTER DATABAE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE  hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
