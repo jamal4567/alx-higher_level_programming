fcf9bb7ebfdeacb8b83d2ea8a3dd0556f2e0104b
jamal <jamalimmiss97@gmail.com>
2024-01-14 22:56:53 +0000

    task-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-''' script that lists all states from the database hbtn_0e_0_usa '''
-
+'''
+ script that lists all states with a name,
+ starting with N (upper N) from the database hbtn_0e_0_usa
+'''
 
 import MySQLdb
 from sys import argv
 
-"""
+'''
 Access to the database and get the states
 from the database.
-"""
+'''
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     conn = MySQLdb.connect(
         host = "localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
