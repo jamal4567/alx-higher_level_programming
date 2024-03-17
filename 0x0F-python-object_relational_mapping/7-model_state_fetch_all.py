ad3afe6a1e9645bc29b846a2c191f57f012b15ee
jamal <jamalimmiss97@gmail.com>
2024-03-17 23:20:03 +0000

    task-7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}' \
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
             .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
