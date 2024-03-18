0799cc1854fce99447709ba603ae34d052ec2de3
jamal <jamalimmiss97@gmail.com>
2024-03-18 01:02:11 +0000

    task14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,10 +3,8 @@
 Script that defines class states
 """
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
 
 
 class State(Base):
@@ -14,8 +12,10 @@ class State(Base):
     __tablename__ : Represent table of the class.
     id (int): Represent the id of the class.
     name (str): Represent state name of the class.
+    state_id (int): The state the city belongs to
     """
-    __tablename__ = "states"
+    __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
